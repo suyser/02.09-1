65111cc5f4a9408e37c9676935daa7bbb3227fac
Кристина <@DESKTOP-UCF1446>
2024-09-08 21:34:22 +0700

    Добавьте файлы проекта.

---
+++ 02.09-1/CustomConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._09_1
+{
+    internal class CustomConverter
+    {
+        public void ConvertToInt(string input, out int result)
+        {
+            result = Convert.ToInt32(input);
+        }
+
+        public void ConvertToDouble(string input, out double result)
+        {
+            if (!double.TryParse(input, NumberStyles.Any, CultureInfo.InvariantCulture, out result))
+            {
+                throw new FormatException($"Input string '{input}' was not in a correct format.");
+            }
+        }
+
+        public void ConvertToBool(string input, out bool result)
+        {
+            result = Convert.ToBoolean(input);
+        }
+
+        public void ConvertToDateTime(string input, out DateTime result)
+        {
+            result = Convert.ToDateTime(input);
+        }
+
+        public void ConvertToLong(string input, out long result)
+        {
+            result = Convert.ToInt64(input);
+        }
+    }
+}
---
+++ 02.09-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        _02._09_1.CustomConverter converter = new _02._09_1.CustomConverter();
+
+        string numberString = "123";
+        converter.ConvertToInt(numberString, out int intValue);
+        Console.WriteLine($"Converted to int: {intValue}");
+
+        string doubleString = "123.45";
+        converter.ConvertToDouble(doubleString, out double doubleValue);
+        Console.WriteLine($"Converted to double: {doubleValue}");
+
+        string boolString = "true";
+        converter.ConvertToBool(boolString, out bool boolValue);
+        Console.WriteLine($"Converted to bool: {boolValue}");
+
+        string dateString = "2023-10-01";
+        converter.ConvertToDateTime(dateString, out DateTime dateValue);
+        Console.WriteLine($"Converted to DateTime: {dateValue}");
+
+        string longString = "1234567890123456789";
+        converter.ConvertToLong(longString, out long longValue);
+        Console.WriteLine($"Converted to long: {longValue}");
+    }
+}
